aa78d2c0468d908d32eff77f6cad5bc884b10043
Sergey Biloshkurskyi <biloshkurskyi.ss@gmail.com>
2017-07-09 16:21:07 +0300

    Added files.

---
+++ Notes/MVVM way/MVVM 1st.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class DynamicString {
+    var value: String
+    
+    init(_ v: String) {
+        value = v
+    }
+}
+
+class
