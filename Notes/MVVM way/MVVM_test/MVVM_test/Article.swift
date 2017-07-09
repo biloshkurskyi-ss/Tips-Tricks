46963e0d9c720af9709ccbee9f1a5b94f9d1ddb4
Sergey Biloshkurskyi <biloshkurskyi.ss@gmail.com>
2017-07-09 18:03:41 +0300

    Updated MVVM example

---
+++ Notes/MVVM way/MVVM_test/MVVM_test/Article.swift
@@ -0,0 +1,16 @@
+//
+//  Article.swift
+//  MVVM_test
+//
+//  Created by Sergey Biloshkurskyi on 7/9/17.
+//  Copyright © 2017 Sergey Biloshkurskyi. All rights reserved.
+//
+
+import Foundation
+
+class Article {
+    var title: String!
+    var body: String!
+    var date: Date!
+    var url: URL!
+}
--- Notes/MVVM way/MVVM_test/MVVM_test/ViewController.swift
+++ Notes/MVVM way/MVVM_test/MVVM_test/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var bodyTextLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    var viewModel: ArticleViewViewModel! {
+        didSet {
+            viewModel.title.bindAndFire { [unowned self] in
+                self.titleLabel.text = $0
+            }
+            viewModel.body.bindAndFire { [unowned self] in
+                self.bodyTextLabel.text = $0
+            }
+            viewModel.date.bindAndFire { [unowned self] in
+                self.dateLabel.text = $0
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let article = Article()
+        article.title = "Title"
+        article.body = "Body"
+        article.date = Date()
+        article.url = URL(string: "https://github.com/StackExchange/stack-blog/blob/master/images/adding_authors.png")
+        
+        let viewModel = ArticleViewViewModelFromView(article)
+        self.viewModel = viewModel
     }
 
     override func didReceiveMemoryWarning() {
