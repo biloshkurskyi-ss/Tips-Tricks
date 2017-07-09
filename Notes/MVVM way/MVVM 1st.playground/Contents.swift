9dbfed4a70b19d6119e2f3e1a4611a587abace27
Sergey Biloshkurskyi <biloshkurskyi.ss@gmail.com>
2017-07-09 16:43:29 +0300

    Added MVVM playground with Generyc processing.

--- Notes/MVVM way/MVVM 1st.playground/Contents.swift
+++ Notes/MVVM way/MVVM 1st.playground/Contents.swift
@@ -2,12 +2,50 @@
 
 import UIKit
 
-class DynamicString {
-    var value: String
+class Dynamic<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
     
-    init(_ v: String) {
+    func bind(listener: Listener?) {
+        self.listener = listener
+    }
+    
+    func bindAndFire(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ v: T) {
         value = v
     }
 }
 
-class
+
+let label = UILabel()
+let name = Dynamic("Steve")
+
+name.bind {
+    label.text = $0
+    print("label.text = \(label.text)")
+}
+
+print(name.value)
+name.value = "Tim"
+print(name.value)
+name.value = "Bob"
+
+
+let alive = Dynamic(true)
+alive.bindAndFire(listener: {
+    print("alive = \($0)")
+})
+let products = Dynamic(["Macintosh","iPhone"])
+products.bindAndFire(listener: {
+    print("products = \($0)")
+})
