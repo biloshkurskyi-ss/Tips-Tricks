8506ef8020a9e0a9df4b86e2267b54892b5ec85c
Sergey Biloshkurskyi <biloshkurskyi.ss@gmail.com>
2017-07-13 22:40:55 +0300

    Finish realization.

--- Notes/MVVM_way/MVVMSwiftExample-Start/MVVMSwiftExample/GameScoreboardEditorViewModel.swift
+++ Notes/MVVM_way/MVVMSwiftExample-Start/MVVMSwiftExample/GameScoreboardEditorViewModel.swift
@@ -11,12 +11,14 @@ import Foundation
 protocol GameScoreboardEditorViewModel {
     var homeTeam: String { get }
     var awayTeam: String { get }
-    var time: String { get }
-    var score: String { get }
-    var isFinished: Bool { get }
+    var time: Dynamic<String> { get }
+    var score: Dynamic<String> { get }
+    var isFinished: Dynamic<Bool> { get }
+    
+    var isPaused: Dynamic<Bool> { get }
+    
     var homePlayers: [PlayerScoreboardMoveEditorViewModel] { get }
     var awayPlayers: [PlayerScoreboardMoveEditorViewModel] { get }
     
-    var isPaused: Bool { get }
     func togglePause();
 }
